ad05edbbf5cbe0f3d0c36a4f1bc65cc704beb6d8
rocky-191 <1375022007@qq.com>
2016-10-17 15:30:03 +0800

    add swipeLeft.html

---
+++ style/js/page/swipeLeft.js
@@ -0,0 +1,18 @@
+(function () {	
+    //document.addEventListener('DOMContentLoaded', init, false);
+	init();
+})();
+
+window.onload=function(){
+	FastClick.attach(document.body);
+	
+	//侧滑
+	$(".item").on("swipeleft",function(){
+        $(this).addClass('selected').parents(".touch").siblings().find(".item").removeClass('selected');
+        $(this).find("a.confirm").on("click",function(){
+            alert("hello");
+        });
+    }).on("swiperight",function(){
+        $(this).parents(".touch").find(".item").removeClass('selected');
+    });
+};
