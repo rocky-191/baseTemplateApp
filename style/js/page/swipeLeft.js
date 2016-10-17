192214c49150af80583831da88d6fc520fc9a1b6
rocky-191 <1375022007@qq.com>
2016-10-17 15:47:57 +0800

    edit  swipeLeft.js

--- style/js/page/swipeLeft.js
+++ style/js/page/swipeLeft.js
@@ -9,8 +9,9 @@ window.onload=function(){
 	//侧滑
 	$(".item").on("swipeleft",function(){
         $(this).addClass('selected').parents(".touch").siblings().find(".item").removeClass('selected');
-        $(this).find("a.confirm").on("click",function(){
-            alert("hello");
+        $(this).find("a.js-del").on("click",function(){
+            var self=this;
+    		$(self).parent().parent().parent().remove();
         });
     }).on("swiperight",function(){
         $(this).parents(".touch").find(".item").removeClass('selected');
