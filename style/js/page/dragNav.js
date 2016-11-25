c9d996507be536c4b2367aa3722700c3456e17b2
rocky-191 <1375022007@qq.com>
2016-11-25 13:18:20 +0800

    edit dragNav.js

--- style/js/page/dragNav.js
+++ style/js/page/dragNav.js
@@ -22,22 +22,24 @@ window.onload=function(){
 	div.addEventListener('touchmove', function(event) {
 		event.preventDefault();//阻止其他事件
 		// 如果这个元素的位置内只有一个手指的话
+		var windowWidth=window.width,
+			windowHeight=window.height;
 		if (event.targetTouches.length == 1) {
 			var touch = event.targetTouches[0];  // 把元素放在手指所在的位置
 			
-			if(touch.pageX>=($(window).width()-50)){
-				div.style.left = $(window).width()-50 + 'px';
+			if(touch.pageX>=(windowWidth-50)){
+				div.style.left = windowWidth-50 + 'px';
 				div.style.top = touch.pageY + 'px';
-			}else if(touch.pageY>=($(window).height()-50)){
+			}else if(touch.pageY>=(windowHeight-50)){
 				div.style.left = touch.pageX + 'px';
-				div.style.top = $(window).height()-50 + 'px';
+				div.style.top = windowHeight-50 + 'px';
 			}else {
 				div.style.left = touch.pageX + 'px';
 				div.style.top = touch.pageY + 'px';
 			}
-			if(touch.pageY>=($(window).height()-50) && touch.pageX>=($(window).width()-50)){
-				div.style.left = $(window).width()-50 + 'px';
-				div.style.top = $(window).height()-50 + 'px';
+			if(touch.pageY>=(windowHeight-50) && touch.pageX>=(windowWidth-50)){
+				div.style.left = windowWidth-50 + 'px';
+				div.style.top = windowHeight-50 + 'px';
 			}
 		}
 	}, false);
