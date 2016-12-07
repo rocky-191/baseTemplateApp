4a67820ff7840f0a6abdf8833758b40fb74ff2bb
rocky191 <1375022007@qq.com>
2016-12-07 14:52:29 +0800

    modify login.html

--- style/js/page/login.js
+++ style/js/page/login.js
@@ -1,4 +1,18 @@
-(function () {	
-    //document.addEventListener('DOMContentLoaded', init, false);
-	init();
+(function(){
+	//active处理兼容，防止active伪类失效
+	document.addEventListener('touchstart',function(){},false);
 })();
+
+window.onload=function(){
+	//用canvas加载背景图，可以加快加载速度
+//	var cxt=document.getElementById("bgCanvas").getContext("2d");
+//	var imageObj = new Image();
+//	var w=document.body.clientWidth,
+//		h=document.body.clientHeight;
+//	cxt.width =w;  
+//  cxt.height=h; 
+//	imageObj.onload = function(){
+//	  cxt.drawImage(imageObj,0,0);
+//	};
+//	imageObj.src = 'style/image/loginBg.png';
+}
